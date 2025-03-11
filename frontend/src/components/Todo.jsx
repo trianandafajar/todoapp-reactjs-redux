5945a5253d1df77461b7ec8e856bd472770c3479
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-11 10:08:19 +0700

    Update Todo

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -30,7 +30,7 @@ const Todo = () => {
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
-        TODO APP
+        Todo APP
       </h2>
       <div className="flex items-center mb-4">
         <input

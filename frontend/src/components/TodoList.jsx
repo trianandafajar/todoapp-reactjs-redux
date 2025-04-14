b3283d546555b5a81a6563b6016f00b3cf8bfb87
trianandafajar <mail.trianandafajar@gmail.com>
2025-04-14 11:57:35 +0700

    Updata Todo List

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllTodos } from "../features/TotoSlice.js";
+import { getAllTodos } from "../features/TodoSlice.js";
 import TodoItem from "./TodoItem.jsx";
 
 const TodoList = () => {
@@ -13,15 +13,29 @@ const TodoList = () => {
     dispatch(getAllTodos());
   }, [dispatch]);
 
+  if (loading) {
+    return (
+      <div className="text-center py-4">
+        <span className="text-blue-500">Loading...</span>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="text-center py-4">
+        <span className="text-red-500">Error: {error}</span>
+      </div>
+    );
+  }
+
   return (
     <ul>
-      <li className="my-2 text-sm italic">All Your Notes Here...</li>
-      {loading && <li>Loading...</li>}
-      {error && <li>{error}</li>}
-      {todos &&
-        todos.map((todo, index) => (
-          <TodoItem key={todo.id} todo={todo} index={index} />
-        ))}
+      {todos.length === 0 ? (
+        <li className="my-2 text-sm text-gray-500 italic">No todos available...</li>
+      ) : (
+        todos.map((todo, index) => <TodoItem key={todo.id} todo={todo} index={index} />)
+      )}
     </ul>
   );
 };
